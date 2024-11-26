757821e33ba925ca36c33ed194529eba86a34537
Lambin Simon <simon.lamb98@gmail.com>
2024-11-26 18:19:32 +0100

    feat: add the entity module's stubs

--- gazu-stubs/shot.pyi
+++ gazu-stubs/shot.pyi
@@ -1,68 +1,42 @@
-from typing import Any, Literal, TypedDict
+from typing import Literal
 
 from .client import KitsuClient, default_client
+from .entity import EntityDict
+from .project import ProjectDict
 
-class ShotDict(TypedDict):
-    canceled: bool
-    data: dict[str, Any]
-    description: str | None
-    entity_type_id: str
+class ShotDict(EntityDict):
     episode_id: str | None
     episode_name: str
     fps: float | None
     frame_in: int | None
     frame_out: int | None
-    id: str
-    name: str
     nb_frames: int
-    parent_id: str
-    preview_file_id: str
-    project_id: str
     project_name: str
     sequence_id: str
     sequence_name: str
-    source_id: str | None
-    nb_entities_out: int
-    is_casting_standby: bool
     # tasks: ?
     type: Literal["Shot"]
-    code: str | None
-    shotgun_id: str | None
-    status: Literal["running"]
-    ready_for: str | None
-    created_by: str
-    entities_out: list[str]
-    entity_concept_links: list[str]
-    instance_casting: list[str]
-    created_at: str
-    updated_at: str
-    entities_in: list[str]
 
-class SequenceDict(TypedDict):
-    id: str
-    name: str
-    code: str | None
-    description: str
-    shotgun_id: str | None
-    canceled: bool
+class SequenceDict(EntityDict):
     nb_frames: int | None
-    nb_entities_out: int
-    is_casting_standby: bool
-    status: Literal["running"]
-    project_id: str
-    entity_type_id: str
-    parent_id: str | None
-    source_id: str | None
-    preview_file_id: str | None
-    data: dict[str, Any]
-    ready_for: str | None
-    created_by: str
-    created_at: str
-    updated_at: str
-    type: Literal["Sequence"]
     project_name: str
+    type: Literal["Sequence"]
+
+class EpisodeDict(EntityDict):
+    project_name: str
+    type: Literal["Episode"]
 
 def get_shot(shot_id: str, client: KitsuClient = default_client) -> ShotDict: ...
+def get_shot_by_name(sequence: str | SequenceDict, shot_name: str, client: KitsuClient = default_client) -> ShotDict: ...
 def get_sequence(
     sequence_id: str, client: KitsuClient = default_client
 ) -> SequenceDict: ...
+def get_sequence_by_name(
+    project: str | ProjectDict, sequence_name: str, episode: str | EpisodeDict | None = None, client: KitsuClient = default_client
+) -> SequenceDict: ...
+def get_episode(
+    episode_id: str, client: KitsuClient = default_client
+) -> EpisodeDict: ...
+def get_episode_by_name(
+    project: str | ProjectDict, episode_name: str, client: KitsuClient = default_client
+) -> EpisodeDict: ...
