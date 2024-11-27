62153b24afead16fc5f4bb39b12868a61f6def39
Lambin Simon <simon.lamb98@gmail.com>
2024-11-27 16:53:37 +0100

    feat: add some client method typing

--- gazu-stubs/client.pyi
+++ gazu-stubs/client.pyi
@@ -1,4 +1,20 @@
+from typing import Any
+
 class KitsuClient:
     pass
 
 default_client: KitsuClient
+
+def fetch_all(
+    path: str,
+    params: dict[Any, Any] | None = None,
+    client: KitsuClient = default_client,
+    paginated: bool = False,
+    limit: int | None = None,
+) -> list[dict[Any, Any]]: ...
+def fetch_first(
+    path: str, params: dict[Any, Any] | None = None, client: KitsuClient = default_client
+) -> dict[Any, Any] | None: ...
+def fetch_one(
+    model_name: str, id: str, client: KitsuClient = default_client
+) -> dict[Any, Any] | None: ...
