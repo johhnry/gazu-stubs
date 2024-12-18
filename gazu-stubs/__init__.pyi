ee746ce1b5fb63419e6d0c1e56f1e9d599c4813e
Joseph HENRY <joseph@welcometolove.tv>
2024-12-18 16:37:01 +0100

    add exception and helpers

--- gazu-stubs/__init__.pyi
+++ gazu-stubs/__init__.pyi
@@ -1,13 +1,22 @@
 from . import (
     asset,  # noqa: F401
+    cache,  # noqa: F401
     casting,  # noqa: F401
     client,
     entity,  # noqa: F401
     events,  # noqa: F401
+    exception,  # noqa: F401
+    helpers,
     person,  # noqa: F401
     project,  # noqa: F401
     shot,  # noqa: F401
     task,  # noqa: F401
+)
+from . import client as raw  # noqa: F401
+from .exception import (
+    AuthFailedException,  # noqa: F401
+    NotAuthenticatedException,  # noqa: F401
+    ParameterException,  # noqa: F401
 )
 
 def set_host(url: str, client: client.KitsuClient = client.default_client) -> None: ...
