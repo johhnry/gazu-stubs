c68ad75422a5d1702c88750bca217254ac810697
Joseph HENRY <joseph@autourdeminuit.com>
2025-10-15 12:30:52 +0200

    refactor use literal native union

--- gazu-stubs/entity.pyi
+++ gazu-stubs/entity.pyi
@@ -17,12 +17,7 @@ class EntityDict(TypedDict):
     is_casting_standby: bool
     code: str | None
     shotgun_id: str | None
-    status: (
-        Literal["standby"]
-        | Literal["running"]
-        | Literal["complete"]
-        | Literal["canceled"]
-    )
+    status: Literal["standby", "running", "complete", "canceled"]
     ready_for: str | None
     created_by: str
     entities_out: list[str]
